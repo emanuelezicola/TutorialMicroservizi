38f5c72c7d0800ba3626fbfa571f41c80714cac6
Emanuele Zicola <emanuelezicola95@gmail.com>
2022-07-26 23:51:00 +0200

    Registrato ogni microservizio su eureka

--- RatingsData/src/main/java/it/projectalpha/ratingsdata/RatingsDataApplication.java
+++ RatingsData/src/main/java/it/projectalpha/ratingsdata/RatingsDataApplication.java
@@ -2,8 +2,10 @@ package it.projectalpha.ratingsdata;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
+@EnableEurekaClient
 public class RatingsDataApplication {
 
     public static void main(String[] args) {
