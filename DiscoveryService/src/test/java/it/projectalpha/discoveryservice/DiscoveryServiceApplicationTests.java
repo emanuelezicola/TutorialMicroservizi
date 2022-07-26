c4af357f17d2ad94d75ca5c01f740bceaffb6d09
Emanuele Zicola <emanuelezicola95@gmail.com>
2022-07-26 23:32:52 +0200

    Aggiunto Service Discovery con Eureka

---
+++ DiscoveryService/src/test/java/it/projectalpha/discoveryservice/DiscoveryServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package it.projectalpha.discoveryservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DiscoveryServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
